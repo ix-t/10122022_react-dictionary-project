25f0b517c6d7759e7765c9aafd0e9baf02cbfa7d
ix-t <107044773+ix-t@users.noreply.github.com>
2022-10-13 12:27:01 -0500

    API integration added
    - Axios added
    - API calls tested
    - App css style updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello</header>
+        <header className="app-header">Dictionary</header>
         <main>
           <Dictionary />
         </main>
