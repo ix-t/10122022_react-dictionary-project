079509b4aaaf34e9482a7036956e13c04ed40865
ix-t <107044773+ix-t@users.noreply.github.com>
2022-10-19 16:54:52 -0500

    Phonetic component updated
    - rel="noreferrer" added in link

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
   if (props.phonetic.audio !== "") {
     return (
       <div className="phonetic">
-        <a href={props.phonetic.audio} target="_blank">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
           Listen
         </a>
         <p>{props.phonetic.text}</p>
